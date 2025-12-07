b0cc48d573260d9ce6cf54a9a55487a4d0307233
Popsie Andal <andalpopsie@gmail.com>
2025-12-07 10:53:32 +0800

    Remove debug logs - comments now show correct names

--- GoalStreakApp/src/screens/SocialScreen.tsx
+++ GoalStreakApp/src/screens/SocialScreen.tsx
@@ -227,11 +227,7 @@ export default function SocialScreen() {
               activityFeed={activityFeed}
               onReaction={handleReaction}
               currentUserId={user?.id}
-              currentUserName={(() => {
-                const name = user?.displayName || user?.email?.split('@')[0] || 'User';
-                console.log('🔍 Passing currentUserName:', name, 'from user:', user);
-                return name;
-              })()}
+              currentUserName={user?.displayName || user?.email?.split('@')[0] || 'User'}
             />
           )}
 
